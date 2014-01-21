8fe38475dfae2cf3801753be7ca5f618d4fb6c9b
Brendan Hay <brendan.g.hay@gmail.com>
2014-01-21 15:13:57 +0100

    Adding instance type documentation

--- lib/src/Network/AWS/Internal/Types/Common.hs
+++ lib/src/Network/AWS/Internal/Types/Common.hs
@@ -111,59 +111,437 @@ instance ToXML AvailabilityZone where
 
 data InstanceType
     = T1_Micro
-      -- ^ @Architecture:@ 32-bit/64-bit, @vCPU:@ 1, @ECPU:@ Variable, @Memory (GiB):@ 0.615, @Instance Storage (GB):@ EBS only, @EBS Optimised:@ -, @Network Performance:@ Very Low
+      -- ^
+      --
+      -- * @Architecture:@ 32-bit/64-bit
+      --
+      -- * @vCPU:@ 1
+      --
+      -- * @ECPU:@ Variable
+      --
+      -- * @Memory (GiB):@ 0.615
+      --
+      -- * @Instance Storage (GB):@ EBS only
+      --
+      -- * @EBS Optimised:@ -
+      --
+      -- * @Network Performance:@ Very Low
     | M3_XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 4, @ECPU:@ 13, @Memory (GiB):@ 15, @Instance Storage (GB):@ 2 x 40 SSD, @EBS Optimised:@ Yes, @Network Performance:@ Moderate
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 4
+      --
+      -- * @ECPU:@ 13
+      --
+      -- * @Memory (GiB):@ 15
+      --
+      -- * @Instance Storage (GB):@ 2 x 40 SSD
+      --
+      -- * @EBS Optimised:@ Yes
+      --
+      -- * @Network Performance:@ Moderate
     | M3_2XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 8, @ECPU:@ 26, @Memory (GiB):@ 30, @Instance Storage (GB):@ 2 x 80 SSD, @EBS Optimised:@ Yes, @Network Performance:@ High
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 8
+      --
+      -- * @ECPU:@ 26
+      --
+      -- * @Memory (GiB):@ 30
+      --
+      -- * @Instance Storage (GB):@ 2 x 80 SSD
+      --
+      -- * @EBS Optimised:@ Yes
+      --
+      -- * @Network Performance:@ High
     | M1_Small
-      -- ^ @Architecture:@ 32-bit/64-bit, @vCPU:@ 11, @ECPU:@ 1, @Memory (GiB):@ 1.7, @Instance Storage (GB):@ 1 x 160, @EBS Optimised:@ -, @Network Performance:@ Low
+      -- ^
+      --
+      -- * @Architecture:@ 32-bit/64-bit
+      --
+      -- * @vCPU:@ 11
+      --
+      -- * @ECPU:@ 1
+      --
+      -- * @Memory (GiB):@ 1.7
+      --
+      -- * @Instance Storage (GB):@ 1 x 160
+      --
+      -- * @EBS Optimised:@ -
+      --
+      -- * @Network Performance:@ Low
     | M1_Medium
-      -- ^ @Architecture:@ 32-bit/64-bit, @vCPU:@ 1, @ECPU:@ 2, @Memory (GiB):@ 3.75, @Instance Storage (GB):@ 1 x 410, @EBS Optimised:@ -, @Network Performance:@ Moderate
+      -- ^
+      --
+      -- * @Architecture:@ 32-bit/64-bit
+      --
+      -- * @vCPU:@ 1
+      --
+      -- * @ECPU:@ 2
+      --
+      -- * @Memory (GiB):@ 3.75
+      --
+      -- * @Instance Storage (GB):@ 1 x 410
+      --
+      -- * @EBS Optimised:@ -
+      --
+      -- * @Network Performance:@ Moderate
     | M1_Large
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 2, @ECPU:@ 4, @Memory (GiB):@ 7.5, @Instance Storage (GB):@ 2 x 420, @EBS Optimised:@ Yes, @Network Performance:@ Moderate
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 2
+      --
+      -- * @ECPU:@ 4
+      --
+      -- * @Memory (GiB):@ 7.5
+      --
+      -- * @Instance Storage (GB):@ 2 x 420
+      --
+      -- * @EBS Optimised:@ Yes
+      --
+      -- * @Network Performance:@ Moderate
     | M1_XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 4, @ECPU:@ 8, @Memory (GiB):@ 15, @Instance Storage (GB):@ 4 x 420, @EBS Optimised:@ Yes, @Network Performance:@ High
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 4
+      --
+      -- * @ECPU:@ 8
+      --
+      -- * @Memory (GiB):@ 15
+      --
+      -- * @Instance Storage (GB):@ 4 x 420
+      --
+      -- * @EBS Optimised:@ Yes
+      --
+      -- * @Network Performance:@ High
     | C3_Large
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 2, @ECPU:@ 7, @Memory (GiB):@ 3.75, @Instance Storage (GB):@ 2 x 16 SSD, @EBS Optimised:@ -, @Network Performance:@ Moderate
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 2
+      --
+      -- * @ECPU:@ 7
+      --
+      -- * @Memory (GiB):@ 3.75
+      --
+      -- * @Instance Storage (GB):@ 2 x 16 SSD
+      --
+      -- * @EBS Optimised:@ -
+      --
+      -- * @Network Performance:@ Moderate
     | C3_XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 4, @ECPU:@ 14, @Memory (GiB):@ 7.5, @Instance Storage (GB):@ 2 x 40 SSD, @EBS Optimised:@ Yes, @Network Performance:@ Moderate
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 4
+      --
+      -- * @ECPU:@ 14
+      --
+      -- * @Memory (GiB):@ 7.5
+      --
+      -- * @Instance Storage (GB):@ 2 x 40 SSD
+      --
+      -- * @EBS Optimised:@ Yes
+      --
+      -- * @Network Performance:@ Moderate
     | C3_2XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 8, @ECPU:@ 28, @Memory (GiB):@ 15, @Instance Storage (GB):@ 2 x 80 SSD, @EBS Optimised:@ Yes, @Network Performance:@ High
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 8
+      --
+      -- * @ECPU:@ 28
+      --
+      -- * @Memory (GiB):@ 15
+      --
+      -- * @Instance Storage (GB):@ 2 x 80 SSD
+      --
+      -- * @EBS Optimised:@ Yes
+      --
+      -- * @Network Performance:@ High
     | C3_4XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 16, @ECPU:@ 55, @Memory (GiB):@ 30, @Instance Storage (GB):@ 2 x 160 SSD, @EBS Optimised:@ Yes, @Network Performance:@ High
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 16
+      --
+      -- * @ECPU:@ 55
+      --
+      -- * @Memory (GiB):@ 30
+      --
+      -- * @Instance Storage (GB):@ 2 x 160 SSD
+      --
+      -- * @EBS Optimised:@ Yes
+      --
+      -- * @Network Performance:@ High
     | C3_8XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 32, @ECPU:@ 108, @Memory (GiB):@ 60, @Instance Storage (GB):@ 2 x 320 SSD, @EBS Optimised:@ -, @Network Performance:@ 10 Gigabit
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 32
+      --
+      -- * @ECPU:@ 108
+      --
+      -- * @Memory (GiB):@ 60
+      --
+      -- * @Instance Storage (GB):@ 2 x 320 SSD
+      --
+      -- * @EBS Optimised:@ -
+      --
+      -- * @Network Performance:@ 10 Gigabit
     | C1_Medium
-      -- ^ @Architecture:@ 32-bit/64-bit, @vCPU:@ 2, @ECPU:@ 5, @Memory (GiB):@ 1.7, @Instance Storage (GB):@ 1 x 350, @EBS Optimised:@ -, @Network Performance:@ Moderate
+      -- ^
+      --
+      -- * @Architecture:@ 32-bit/64-bit
+      --
+      -- * @vCPU:@ 2
+      --
+      -- * @ECPU:@ 5
+      --
+      -- * @Memory (GiB):@ 1.7
+      --
+      -- * @Instance Storage (GB):@ 1 x 350
+      --
+      -- * @EBS Optimised:@ -
+      --
+      -- * @Network Performance:@ Moderate
     | C1_XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 8, @ECPU:@ 20, @Memory (GiB):@ 7, @Instance Storage (GB):@ 4 x 420, @EBS Optimised:@ Yes, @Network Performance:@ High
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 8
+      --
+      -- * @ECPU:@ 20
+      --
+      -- * @Memory (GiB):@ 7
+      --
+      -- * @Instance Storage (GB):@ 4 x 420
+      --
+      -- * @EBS Optimised:@ Yes
+      --
+      -- * @Network Performance:@ High
     | CC2_8XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 32, @ECPU:@ 88, @Memory (GiB):@ 60.5, @Instance Storage (GB):@ 4 x 840, @EBS Optimised:@ -, @Network Performance:@ 10 Gigabit
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 32
+      --
+      -- * @ECPU:@ 88
+      --
+      -- * @Memory (GiB):@ 60.5
+      --
+      -- * @Instance Storage (GB):@ 4 x 840
+      --
+      -- * @EBS Optimised:@ -
+      --
+      -- * @Network Performance:@ 10 Gigabit
     | G2_2XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 8, @ECPU:@ 26, @Memory (GiB):@ 15, @Instance Storage (GB):@ 1 x 60 SSD, @EBS Optimised:@ Yes, @Network Performance:@ High
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 8
+      --
+      -- * @ECPU:@ 26
+      --
+      -- * @Memory (GiB):@ 15
+      --
+      -- * @Instance Storage (GB):@ 1 x 60 SSD
+      --
+      -- * @EBS Optimised:@ Yes
+      --
+      -- * @Network Performance:@ High
     | CG1_4XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 16, @ECPU:@ 33.5, @Memory (GiB):@ 22.5, @Instance Storage (GB):@ 2 x 840, @EBS Optimised:@ -, @Network Performance:@ 10 Gigabit
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 16
+      --
+      -- * @ECPU:@ 33.5
+      --
+      -- * @Memory (GiB):@ 22.5
+      --
+      -- * @Instance Storage (GB):@ 2 x 840
+      --
+      -- * @EBS Optimised:@ -
+      --
+      -- * @Network Performance:@ 10 Gigabit
     | M2_XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 2, @ECPU:@ 6.5, @Memory (GiB):@ 17.1, @Instance Storage (GB):@ 1 x 420, @EBS Optimised:@ -, @Network Performance:@ Moderate
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 2
+      --
+      -- * @ECPU:@ 6.5
+      --
+      -- * @Memory (GiB):@ 17.1
+      --
+      -- * @Instance Storage (GB):@ 1 x 420
+      --
+      -- * @EBS Optimised:@ -
+      --
+      -- * @Network Performance:@ Moderate
     | M2_2XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 4, @ECPU:@ 13, @Memory (GiB):@ 34.2, @Instance Storage (GB):@ 1 x 850, @EBS Optimised:@ Yes, @Network Performance:@ Moderate
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 4
+      --
+      -- * @ECPU:@ 13
+      --
+      -- * @Memory (GiB):@ 34.2
+      --
+      -- * @Instance Storage (GB):@ 1 x 850
+      --
+      -- * @EBS Optimised:@ Yes
+      --
+      -- * @Network Performance:@ Moderate
     | M2_4XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 8, @ECPU:@ 26, @Memory (GiB):@ 68.4, @Instance Storage (GB):@ 2 x 840, @EBS Optimised:@ Yes, @Network Performance:@ High
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 8
+      --
+      -- * @ECPU:@ 26
+      --
+      -- * @Memory (GiB):@ 68.4
+      --
+      -- * @Instance Storage (GB):@ 2 x 840
+      --
+      -- * @EBS Optimised:@ Yes
+      --
+      -- * @Network Performance:@ High
     | CR1_8XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 32, @ECPU:@ 88, @Memory (GiB):@ 244, @Instance Storage (GB):@ 2 x 120, @EBS Optimised:@ -, @Network Performance:@ 10 Gigabit
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 32
+      --
+      -- * @ECPU:@ 88
+      --
+      -- * @Memory (GiB):@ 244
+      --
+      -- * @Instance Storage (GB):@ 2 x 120
+      --
+      -- * @EBS Optimised:@ -
+      --
+      -- * @Network Performance:@ 10 Gigabit
     | I2_XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 4, @ECPU:@ 14, @Memory (GiB):@ 30.5, @Instance Storage (GB):@ 1 x 800 SSD, @EBS Optimised:@ Yes, @Network Performance:@ Moderate
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 4
+      --
+      -- * @ECPU:@ 14
+      --
+      -- * @Memory (GiB):@ 30.5
+      --
+      -- * @Instance Storage (GB):@ 1 x 800 SSD
+      --
+      -- * @EBS Optimised:@ Yes
+      --
+      -- * @Network Performance:@ Moderate
     | I2_2XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 8, @ECPU:@ 27, @Memory (GiB):@ 61, @Instance Storage (GB):@ 2 x 800 SSD, @EBS Optimised:@ Yes, @Network Performance:@ High
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 8
+      --
+      -- * @ECPU:@ 27
+      --
+      -- * @Memory (GiB):@ 61
+      --
+      -- * @Instance Storage (GB):@ 2 x 800 SSD
+      --
+      -- * @EBS Optimised:@ Yes
+      --
+      -- * @Network Performance:@ High
     | I2_4XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 16, @ECPU:@ 53, @Memory (GiB):@ 122, @Instance Storage (GB):@ 4 x 800 SSD, @EBS Optimised:@ Yes, @Network Performance:@ High
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 16
+      --
+      -- * @ECPU:@ 53
+      --
+      -- * @Memory (GiB):@ 122
+      --
+      -- * @Instance Storage (GB):@ 4 x 800 SSD
+      --
+      -- * @EBS Optimised:@ Yes
+      --
+      -- * @Network Performance:@ High
     | I2_8XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 32, @ECPU:@ 104, @Memory (GiB):@ 244, @Instance Storage (GB):@ 8 x 800 SSD, @EBS Optimised:@ -, @Network Performance:@ 10 Gigabit
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 32
+      --
+      -- * @ECPU:@ 104
+      --
+      -- * @Memory (GiB):@ 244
+      --
+      -- * @Instance Storage (GB):@ 8 x 800 SSD
+      --
+      -- * @EBS Optimised:@ -
+      --
+      -- * @Network Performance:@ 10 Gigabit
     | HS1_8XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 16, @ECPU:@ 35, @Memory (GiB):@ 117, @Instance Storage (GB):@ 24 x 2,0483, @EBS Optimised:@ -, @Network Performance:@ 10 Gigabit
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 16
+      --
+      -- * @ECPU:@ 35
+      --
+      -- * @Memory (GiB):@ 117
+      --
+      -- * @Instance Storage (GB):@ 24 x 2,0483
+      --
+      -- * @EBS Optimised:@ -
+      --
+      -- * @Network Performance:@ 10 Gigabit
     | HI1_4XLarge
-      -- ^ @Architecture:@ 64-bit, @vCPU:@ 16, @ECPU:@ 35, @Memory (GiB):@ 60.5, @Instance Storage (GB):@ 2 x 1,024 SSD, @EBS Optimised:@ -, @Network Performance:@ 10 Gigabit
+      -- ^
+      --
+      -- * @Architecture:@ 64-bit
+      --
+      -- * @vCPU:@ 16
+      --
+      -- * @ECPU:@ 35
+      --
+      -- * @Memory (GiB):@ 60.5
+      --
+      -- * @Instance Storage (GB):@ 2 x 1,024 SSD
+      --
+      -- * @EBS Optimised:@ -
+      --
+      -- * @Network Performance:@ 10 Gigabit
       deriving (Eq, Ord, Generic)
 
 instance FromText InstanceType where
